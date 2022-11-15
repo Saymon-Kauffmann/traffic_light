89c7766e8fce9b7e3219e85e5c782b1145dd752d
Saymon_Kauffmann <101084320+Saymon-Kauffmann@users.noreply.github.com>
2022-11-15 03:30:02 -0300

    project finished
    now I'll try to set specific periods of time to each color

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 const dfImage = document.getElementById('img');
 const buttons = document.getElementById('bttns');
+let intervalId = null;
+let colorIdx = 0; 
 
 const trafficLight = (event) => {
+    intControl();
     activate[event.target.id]();
+    
 }
+
+const nextIdx = () => {
+    // the ++ MUST be in the right side of colorIdx to get the addition before the process start
+    colorIdx = colorIdx  < 2 ? ++colorIdx : 0; 
+}
+
+const automatic = () => {
+    const colors = ['red', 'yellow', 'green'];
+    const color = colors[colorIdx];
+    activate[color]();
+    nextIdx();
+}
+
+// this is necessary to control/limit the speed of the intervals each time that the user click the button
+const intControl = () => clearInterval(intervalId);
 
 const activate = {
     'red': () => dfImage.src = './imgs/red.png',
     'yellow': () => dfImage.src = './imgs/yellow.png',
-    'green': () => dfImage.src = './imgs/green.png'
+    'green': () => dfImage.src = './imgs/green.png',
+    'auto': () => intervalId = setInterval(automatic, 1000)
 }
+
+
 
 buttons.addEventListener('click', trafficLight);
 
